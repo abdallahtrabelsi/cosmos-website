ff823d465524a201ae70d6de4e898c50710ffa44
abdallahtrabelsi <abdallahtrabelsi4@gmail.com>
2024-07-10 14:41:49 +0100

    new

---
+++ animation.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     const svg = document.getElementById('cosmos-logo');
+
+//     svg.addEventListener('mouseover', function() {
+//         svg.style.transition = "transform 0.5s ease";
+//         svg.style.transform = "rotate(20deg)";
+//     });
+
+//     svg.addEventListener('mouseout', function() {
+//         svg.style.transition = "transform 0.5s ease";
+//         svg.style.transform = "rotate(0deg)";
+//     });
+// });
